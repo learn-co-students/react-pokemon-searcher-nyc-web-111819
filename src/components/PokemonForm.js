5e15e7f5343b626dd1aa9a6b7b8e49fda62d6f17
Shakil Abdin <shakil.abdin@gmail.com>
2020-02-04 11:33:25 -0500

    Done.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -13,11 +13,37 @@ class PokemonForm extends React.Component {
     }
   }
 
+  handleSubmit = (e) => {
+    this.setState({
+      name: e.target.name.value,
+      hp: e.target.hp.value,
+      frontUrl: e.target.frontUrl.value,
+      backUrl: e.target.backUrl.value
+    }, () => {
+      fetch('http://localhost:3000/pokemon', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+            body: JSON.stringify({
+                name: this.state.name,
+                sprites: {front: this.state.frontUrl, back: this.state.backUrl},
+                stats: [0, 1, 2, 3, 4, {name: "hp", value: parseInt(this.state.hp)}]
+            })
+        }).then(resp => resp.json())
+        .then(newPoke => this.props.newPokemon(newPoke))
+
+
+
+    })
+  }
+
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
-        <Form onSubmit={this.handleSubmit}>
+        <Form onSubmit={(e) => this.handleSubmit(e)}>
           <Form.Group widths="equal">
             <Form.Input fluid label="Name" placeholder="Name" name="name" />
             <Form.Input fluid label="hp" placeholder="hp" name="hp" />
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -4,17 +4,51 @@ import PokemonForm from './PokemonForm'
 import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
+const API = "http://localhost:3000/pokemon"
+
 class PokemonPage extends React.Component {
+
+
+  state = {
+    pokemons: [],
+    search: ""
+  }
+
+  componentDidMount(){
+    fetch(API).then(resp => resp.json())
+    .then(result => this.setState({
+      pokemons: result
+    }))
+  }
+
+  searchByName = (value) => {
+    console.log(value)
+    this.setState({
+      search: value
+    })
+  }
+
+  newPokemon = (newPoke) => {
+    this.setState(prevState => ({
+      pokemons: {...prevState, newPoke}
+    }))
+  }
+
+
+
   render() {
+    // console.log(this.state.pokemons)
+    let filterPokemon = this.state.pokemons.filter(el => el.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1)
+    console.log(filterPokemon)
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm newPokemon={this.newPokemon} />
         <br />
-        <Search onChange={() => console.log('🤔')} />
+        <Search onChange={(e) => this.searchByName(e.target.value)} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={filterPokemon} />
       </Container>
     )
   }
